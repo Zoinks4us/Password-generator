d83224b9fea91f5c436ef89e9065b9fbcbef4460
Zoinks4us <brown.jordan.charles@icloud.com>
2023-07-19 21:23:37 -0400

    finished defining variables for generatepassword function

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,41 @@
 // Define generate passwaord
 //Prompt user on capitalization, length (8-128), numbers, special characters
 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+
+var specCharacters = [".","\\", ":", ";", "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", "/", "<", ">", "=", "?", "@", "^", "_", "~",];
+
+
+ Math.floor(Math.random() * 10);
+ window.confirm('Click OK to include uppercase characters?')
+ window.confirm('Click OK to include lowercase characters?')
+
+ window.confirm('Click OK to include special characters?')
+ window.confirm('Click OK to include numeric characters?')
+
+function alphaChoice() {
+
+}
+
+function getRandomAlpha() {
+   Math.floor(Math.random() * alphabet.length);
+}
+
+function getRandomSpec() {
+  Math.floor(Math.random() * specCharacters.length);
+}
 
 
 function generatePassword( ) {
-  var passUpperCase = window.alert('Click OK to include uppercase characters?')
-var passLowerCase = window.prompt('Click OK to include lowercase characters?')
-var passLength = window.prompt('How many characters would you like your password to contain?')
-var passSpecial = window.prompt('Click OK to include special characters?')
-var passNumbers = window.prompt('Click OK to include numeric characters?')
-
-if(passUpperCase =)
-
-
-
-  return
+  var passUpperCase = alphabet.toUpperCase[getRandomAlpha()]
+  var passLowerCase = alphabet[getRandomAlpha()]
+  var passLength =  window.prompt('How many characters would you like your password to contain?')
+  var passSpecial = specCharacters[getRandomSpec()]
+  var passNumeric = 
 }
+
+
+  
 
 // Get references to the #generate element
 // This targets the button
@@ -37,4 +57,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // returns a random integer from 0-9
-Math.floor(Math.random() * 10);
